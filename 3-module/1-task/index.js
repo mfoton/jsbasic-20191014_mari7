e1f404e1d915f2c372fa16ccc0c66a133764863d
mfoton <mywebserv@yandex.ru>
2019-11-30 15:51:51 +0100

    Update index.js

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,12 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let userSalary = [];
+  for (let user of data) {
+    if (user.age <= age) {
+      let userData = [user.name, user.balance];
+      userSalary.push(userData);
+    }
+  }
+  return userSalary;
 }
