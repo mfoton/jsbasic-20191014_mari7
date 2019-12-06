e1e1e3b5140a5e721b73810813001699a8378bf1
mfoton <mywebserv@yandex.ru>
2019-12-06 08:15:30 +0100

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let list = document.createElement('ul');
+
+  for (let element of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = element.firstName + " " + element.lastName;
+    list.append(li);
+  }
+  document.body.append(list);
 }
